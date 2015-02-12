4c61aa6acd7e603a165ece178c679a61a18c7b49
Aprilcj <april.jianchen@gmail.com>
2015-02-12 09:46:12 -0500

    fix

--- task8/src/controllor/TestServlet.java
+++ task8/src/controllor/TestServlet.java
@@ -17,6 +17,7 @@ public class TestServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		super.doGet(req, resp);
+		resp.getWriter().println("hello");
 	}
+
 }
